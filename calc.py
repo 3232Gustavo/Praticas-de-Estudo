6e27a2130992cad4105c900d1dc78ed962c1a38f
Gustavo Santana <3232gustavo@gmail.com>
2026-08-11 22:02:08 -0300

    Deu?

--- calc.py
+++ calc.py
@@ -1,8 +1,15 @@
 def somar(a, b):
     return a + b
 
+def multiplicar(a, b):
+    return a * b
+
+
+
+
 def subtrair(a, b):
     return a - b
 
 print(somar(5, 3))
 print(subtrair(10, 4))
+print(multiplicar(4, 3))
